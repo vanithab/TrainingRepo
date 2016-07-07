5693ad51de8bd385a6d56d89ef7f2bb2129fb86f
Training <Training@DIN39000535.corp.capgemini.com>
2016-07-07 10:02:10 +0530

    Initial Commit

---
+++ ProductTrainingGit/src/cap/demo/MainClass.java
@@ -0,0 +1,18 @@
+package cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProductApp prodApp = new ProductApp();
+		prodApp.productDisp();
+		productDispMain();
+	}
+
+	public static void productDispMain() {
+		// TODO Auto-generated method stub
+			System.out.println("Display Product Main 1");
+			System.out.println("Display Product Main 2");
+	}
+	
+}
---
+++ ProductTrainingGit/src/cap/demo/ProductApp.java
@@ -0,0 +1,10 @@
+package cap.demo;
+
+public class ProductApp {
+
+	public void productDisp() {
+		// TODO Auto-generated method stub
+			System.out.println("Display Product Sub");
+	}
+
+}
