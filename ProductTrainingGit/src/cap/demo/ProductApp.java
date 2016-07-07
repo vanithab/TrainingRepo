1d8901cf8bae333a2e77867533611ece1d243694
Training <Training@DIN39000535.corp.capgemini.com>
2016-07-07 10:21:14 +0530

    Display Modified

--- ProductTrainingGit/src/cap/demo/ProductApp.java
+++ ProductTrainingGit/src/cap/demo/ProductApp.java
@@ -4,7 +4,11 @@ public class ProductApp {
 
 	public void productDisp() {
 		// TODO Auto-generated method stub
+		int[] arr = {3,5,8};
 			System.out.println("Display Product Sub");
+			System.out.println("Display Product Sub1 Branch");
+			System.out.println("Array Size: " + arr.length);
+
 	}
 
 }
